1931cfb93e31d9d7df33d36977702293e9a6bdae
Grey Chen <957024640@qq.com>
2019-02-28 18:13:10 +0800

    Initial Commit

---
+++ ARMagic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARMagic
+//
+//  Created by Gelei Chen on 2/28/19.
+//  Copyright © 2019 Gelei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
